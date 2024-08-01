3231f6d091a117de55cf26b6e05defe999495104
Teon54 <mohammadisajjad54@gmail.com>
2024-08-01 20:56:39 +0330

    Task 4. Get a Single Record or Create a New Record. Done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Hash;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -31,7 +32,15 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL;
+        $user = User::firstOrCreate([
+            'name' => $name,
+            'email' => $email
+        ],
+            [
+                'name' => $name,
+                'email' => $email,
+                'password' => Hash::make(fake()->password)
+            ]);
 
         return view('users.show', compact('user'));
     }
